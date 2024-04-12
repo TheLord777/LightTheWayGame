b8fecfe66a0195a3ebdd51f1dc851e2dbbcc0c64
Hamish-Finn <hamishfinn2@gmail.com>
2024-04-12 11:27:10 +0100

    basic player - move + jump

---
+++ src/lightTheWay/components/characters/PlayableCharacter.java
@@ -0,0 +1,9 @@
+package lightTheWay.components.characters;
+
+import processing.core.PVector;
+
+public class PlayableCharacter extends Character{
+    public PlayableCharacter(PVector p, float width) {
+        super(p, width);
+    }
+}
