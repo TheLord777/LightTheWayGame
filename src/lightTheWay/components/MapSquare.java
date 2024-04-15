6d860bd4828ad2f336b8df2f5f6f6df6b09ba4ee
Baoyan Si <sibaoyan@gmail.com>
2024-04-15 10:04:41 +0100

    //

--- src/lightTheWay/components/MapSquare.java
+++ src/lightTheWay/components/MapSquare.java
@@ -1,6 +1,8 @@
 package lightTheWay.components;
 
-public class MapSquare {
+import gamecore.components.GameComponent;
+
+public class MapSquare extends GameComponent {
     private int type;
 
     public MapSquare(int type) {
@@ -13,5 +15,20 @@ public class MapSquare {
 
     public void setType(int type) {
         this.type = type;
+    }
+
+    @Override
+    protected void draw() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    public boolean intersection(GameComponent ge) {
+        return false;
     }
 }
